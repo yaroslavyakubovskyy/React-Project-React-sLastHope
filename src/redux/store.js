6de0864c70217c7a0808903d0345dd3412f9ba9c
Vladislav <139064573+worlems@users.noreply.github.com>
2025-05-07 14:53:46 +0300

    merge

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
+import {authReducer} from "./auth/slice.js";
+
 
 const store = configureStore({
-  reducer: {},
-});
+    reducer:{
+        auth: authReducer,
+    },
+})
+
+export default store

3de6244eaf921c395ce37d16d083b228ba2a7def
Stacy <stacyxberlin@gmail.com>
2025-05-09 15:12:01 +0200

    fix layout

--- src/pages/SharedLayout/SharedLayout.jsx
+++ src/pages/SharedLayout/SharedLayout.jsx
@@ -3,14 +3,19 @@ import { Outlet } from "react-router-dom";
 import { BgImageWrapper } from "../../components/BgImageWrapper/BgImageWrapper.jsx";
 import React, { Fragment } from "react";
 import Media from "react-media";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors.js";
 
 const SharedLayout = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <>
       <Header />
-      <Media query="(min-width: 1440px)">
-        {(matches) => matches && <BgImageWrapper />}
-      </Media>
+      {!isLoggedIn && (
+        <Media query="(min-width: 1440px)">
+          {(matches) => matches && <BgImageWrapper />}
+        </Media>
+      )}
       <Outlet />
     </>
   );

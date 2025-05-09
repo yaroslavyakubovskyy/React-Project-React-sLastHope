3b7cfc810873fb6e69660605c1e30c91c516fad0
Vladislav Zarovniy <vladislavzarovnii13@gmail.com>
2025-05-09 18:32:31 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,7 +19,7 @@ const persistConfig = {
   key: "root",
   version: 1,
   storage,
-  whitelist: ["accessToken", "refreshToken", "sid"],
+  whitelist: ["refreshToken", "sid"],
 };
 
 const persistedReducer = persistReducer(persistConfig, authReducer);

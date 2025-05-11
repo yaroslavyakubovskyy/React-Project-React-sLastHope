611125d4c67db9651b9c4dd8dcf1da7c698a8d16
Vladyslav Avrashov <vladisloveavrashov@gmail.com>
2025-05-11 23:45:33 +0300

    add datepicker.css & move CustomInput

--- src/components/TransactionsSearchTools/TransactionsSearchTools.jsx
+++ src/components/TransactionsSearchTools/TransactionsSearchTools.jsx
@@ -7,7 +7,7 @@ import { useParams } from "react-router-dom";
 import { getTransactions } from "../../redux/transactions/operations";
 import { LuCalendar } from "react-icons/lu";
 import { format } from "date-fns";
-import CustomInput from "../TransactionForm/CustomInput";
+import CustomInput from "../CustomInput/CustomInput";
 
 const TransactionsSearchTools = ({ searchInput, handleSearchInput }) => {
   const [selectedDate, setSelectedDate] = useState(new Date());

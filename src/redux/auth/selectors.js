5b5ba9eeb6206f36a5b50390dac84be6aa344437
Stacy <stacyxberlin@gmail.com>
2025-05-12 15:04:35 +0200

    fix flashing TransactionsChart.jsx

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,6 +1,4 @@
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectUser = (state) => state.auth.user;
-export const selectisRegistered = (state) => state.auth.isRegistered;
-
-// export const selectIsRefreshing = (state) => state.auth.isRegistered;
+export const selectIsLoadingLogin = (state) => state.auth.isLoading;
 export const selectIsRefreshing = (state) => state.auth.isRefreshing;
--- src/redux/user/selectors.js
+++ src/redux/user/selectors.js
@@ -1,4 +1,4 @@
 export const selectUser = (state) => state.user;
-//
+export const selectIsLoadingUser = (state) => state.user.isLoading;
 export const selectAvatarUrl = (state) => state.user.avatarUrl;
 export const selectCurrency = (state) => state.user.currency;

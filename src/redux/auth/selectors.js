f401368d5265a8a5361cbf1d5f2d61bd7a608694
demyanyshyn <taras.demyanyshyn@gmail.com>
2025-05-13 13:31:58 +0300

    fix refreshing

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -2,3 +2,4 @@ export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectUser = (state) => state.auth.user;
 export const selectIsLoadingLogin = (state) => state.auth.isLoading;
 export const selectIsRefreshing = (state) => state.auth.isRefreshing;
+export const selectIsToken = (state) => Boolean(state.auth.refreshToken);
